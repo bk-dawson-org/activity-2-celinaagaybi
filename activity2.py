7112dc7acd0b35ca692ecc1cbe7be801db4beccf
celinaagaybi <celina.agaybi@dawsoncollege.qc.ca>
2024-09-13 23:14:21 +0000

    activity 2

---
+++ activity2.py
@@ -0,0 +1,5 @@
+print (12)
+print (' That \'s' , 'celsius' , " degrees celsius !")
+print(97 + 2 / 12 -18 * 3  %  2)
+y = 97 + 2 / 12 -18 * 3  %  2
+print (round(y, 4))
---
+++ tax.py
@@ -0,0 +1,7 @@
+price = float(input("enter the price of the food"))
+pricetaxincluded = price * 0.18 + price
+print ('This is the price tax included:', pricetaxincluded)
+
+
+x = 'Benham' * 3
+print (x)
